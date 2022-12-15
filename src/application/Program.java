3b91b1a3611a57647efa4ac3939a805b02ed62c6
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2022-12-15 17:22:51 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Position p = new Position(1, 2);
+		System.out.println(p);
 	}
 
 }
