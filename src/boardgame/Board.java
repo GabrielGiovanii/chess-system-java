2e9074666040a7a56c39c813c945248d678358ae
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2022-12-21 15:28:28 -0300

    Implementing possible moves of Rook

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -56,7 +56,7 @@ public class Board {
 	private boolean positionExists(int row, int column) {
 		return row>=0 && row<rows && column>=0 && column<columns;
 	}
-	private boolean positionExists(Position position) {
+	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
 	public boolean thereIsAPiece(Position position) {
